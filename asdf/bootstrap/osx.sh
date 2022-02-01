cc85c1ea71cdd0843dde6023b2b2913b48f94d31
Alex LaFroscia <alex@lafroscia.com>
2022-01-31 20:18:28 -0500

    Remove config files for tools no longer used
    Some of these are in iCloud now, others are just not used at all

--- asdf/bootstrap/osx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Clone `asdf`
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.0
-
-# Install dependencies
-brew install automake autoconf openssl libyaml readline libxslt libtool unixodbc
