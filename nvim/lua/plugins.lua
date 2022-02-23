91c635fc1df01180acafc327dc208bbe4a10992a
Alex LaFroscia <a_lafroscia@apple.com>
2022-02-23 11:56:26 -0500

    nvim: use recommended config for gitsigns
    This plugin used to ship with keybindings by default, but no longer does. This sets up the recommended configuration from that package’s README!

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -75,9 +75,7 @@ return require("packer").startup(function()
   use({
     "lewis6991/gitsigns.nvim",
     requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("gitsigns").setup({})
-    end,
+    config = [[require('config/gitsigns')]],
   })
   use("sindrets/diffview.nvim")
 
